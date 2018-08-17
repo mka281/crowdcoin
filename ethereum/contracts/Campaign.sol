8d6e00d2429ec5071c6e0e6c99b62c26c8143db3
mka <mka281@icloud.com>
2018-08-17 12:44:42 -0700

    Add restricted modifier

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -7,10 +7,15 @@ contract Campaign {
     address recipient;
     bool complete;
   }
-  
+
   address public manager;
   uint public minimumContribution;
   address[] public approvers;
+
+  modifier restricted() {
+    require(msg.sender == manager);
+    _;
+  }
 
   function Campaign(uint minimum) public {
     manager = msg.sender;
