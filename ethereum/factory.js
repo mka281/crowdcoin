9cbf5cc7b20210a68ef2498c4e717cf93fd1cafb
mka <mka281@icloud.com>
2018-08-19 15:18:14 -0700

    Add get information functions to contract and redeploy

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from "./build/CampaignFactory.json";
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  "0xea9b178E140E784782E7eaBe8cA75c997E94D882"
+  "0x54466C6e98CC4B6E8B46e2959d724d6874f8a01f"
 );
 
 export default instance;
