befef605cca41b030b4facc8a69fc58c0146c892
mka <mka281@icloud.com>
2018-08-18 22:03:03 -0700

    Assemble a Header

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
+import Header from "./Header";
 
 export default props => {
   return (
     <div>
-      <h1>Im a header</h1>
+      <Header />
       {props.children}
       <h2>Im a header</h2>
     </div>
