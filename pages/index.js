ec61a8fb03734cccd7b788a5f2251e110b7367f2
mka <mka281@icloud.com>
2018-08-18 21:47:36 -0700

    Render page with Semantic UI elements

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import factory from "../ethereum/factory";
+import { Card, Button } from "semantic-ui-react";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
@@ -8,8 +9,30 @@ class CampaignIndex extends Component {
     return { campaigns };
   }
 
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  }
+
   render() {
-    return <div>{this.props.campaigns[0]}</div>;
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.3.3/semantic.min.css"
+        />
+        <h3>Open Campaigns</h3>
+        {this.renderCampaigns()}
+        <Button content="Create Campaign" icon="add square" primary />
+      </div>
+    );
   }
 }
 
