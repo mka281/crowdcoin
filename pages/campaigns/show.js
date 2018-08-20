f9b9508a26cdb2b536adf961c72bb6ae4b5af7c3
mka <mka281@icloud.com>
2018-08-20 00:59:45 -0700

    Access campaign info from show page

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
+import Layout from "../../components/Layout";
+import Campaign from "../../ethereum/campaign";
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const campaign = Campaign(props.query.address);
+    const summary = await campaign.methods.getSummary().call();
+    console.log(summary);
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      contributorsCount: summary[3],
+      manager: summary[4]
+    };
+  }
+
   render() {
     return <h3>Campaign Show</h3>;
   }
