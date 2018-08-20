8e3fe8f97920b3065f0d61cd817e9708a4e47979
mka <mka281@icloud.com>
2018-08-20 13:31:40 -0700

    Build new request page

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import { Link } from "../../../routes";
+import Layout from "../../../components/Layout";
 
 class RequestIndex extends Component {
   render() {
-    return <h3>Request List</h3>;
+    return (
+      <Layout>
+        <h3>Request List</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary>Add Request</Button>
+          </a>
+        </Link>
+      </Layout>
+    );
   }
 }
 
