d37245c5edaa392a65d8097234cae26b0b7a3223
mka <mka281@icloud.com>
2018-08-20 06:34:10 -0700

    Display a card for each campaign detail

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { Card } from "semantic-ui-react";
 import Layout from "../../components/Layout";
 import Campaign from "../../ethereum/campaign";
+import web3 from "../../ethereum/web3";
 
 class CampaignShow extends Component {
   static async getInitialProps(props) {
@@ -15,9 +17,59 @@ class CampaignShow extends Component {
       manager: summary[4]
     };
   }
+  renderCards() {
+    const {
+      balance,
+      manager,
+      minimumContribution,
+      requestsCount,
+      contributorsCount
+    } = this.props;
+
+    const items = [
+      {
+        header: manager,
+        meta: "Address of Manager",
+        description:
+          "The manager created this campaign and can create requests to withdraw money",
+        style: { overflowWrap: "break-word" }
+      },
+      {
+        header: minimumContribution,
+        meta: "Minimum Contribution (wei)",
+        description:
+          "You must contribute at least this much wei to become an approver"
+      },
+      {
+        header: requestsCount,
+        meta: "Number of Requests",
+        description:
+          "A request tries to withdraw money from the contract. Requests must be approved by approvers"
+      },
+      {
+        header: contributorsCount,
+        meta: "Number of Approvers",
+        description:
+          "Number of people who have already donated to this campaign"
+      },
+      {
+        header: web3.utils.fromWei(balance, "ether"),
+        meta: "Campaign Balance (ether)",
+        description:
+          "The balance is how much money this campaign has left to spend."
+      }
+    ];
+
+    return <Card.Group items={items} />;
+  }
 
   render() {
-    return <h3>Campaign Show</h3>;
+    return (
+      <Layout>
+        <h3>Campaign Details</h3>
+        {this.renderCards()}
+      </Layout>
+    );
   }
 }
 
